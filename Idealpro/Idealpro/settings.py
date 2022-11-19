1d3682c0cf9ff1853a63c5167c4e28180b38e81c
gben <koredeearth@example.com>
2022-11-19 16:03:40 +0100

    lasts

--- Idealpro/Idealpro/settings.py
+++ Idealpro/Idealpro/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'Ideaapp'
 ]
 
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/'
-
+STATIC_URL = 'static/'
+DISABLE_COLLECTSTATIC = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
